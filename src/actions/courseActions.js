5e258d1805cd47a1bb8a4c326707f4ccb7e01f6b
Luka Devic <luka.m.devic@gmail.com>
2018-12-05 21:31:15 +0100

    Added production files to reduce app size

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,7 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 import {beginAjaxCall} from './ajaxStatusActions';
+import {ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses};
